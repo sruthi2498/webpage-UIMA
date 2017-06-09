de6893cf5c1f42809cf1d357943d828fd18e01b0
sruthi2498 <vsruthi98@gmail.com>
2017-06-09 09:33:30 +0530

    Create action.php

--- src/main/php/action.php
+++ src/main/php/action.php
@@ -1,11 +1,11 @@
- <!DOCTYPE html>
+a <!DOCTYPE html>
 <html>
 	<head> <link rel="stylesheet" type="text/css" href="../css/stylesheet.css"></head>
 <body>
 
 <?php
 
-	echo "<a id='sub' href=\"javascript:history.go(-1)\">GO BACK</a><br>";
+	
 
 	$text=$_POST["text"];
 	$filename="/home/sruthi/dev/UIMA-NLP-Platform/MyWorkspace/data/text_files/Apache_UIMA.txt";
@@ -20,7 +20,8 @@
 	 putenv("JAVA_HOME=$JAVA_HOME");
 	 putenv("PATH=$PATH");
 	$output=shell_exec('cd /home/sruthi/dev/UIMA-NLP-Platform/MyWorkspace && mvn assembly:assembly -DdescriptorId=jar-with-dependencies && java -cp /home/sruthi/dev/UIMA-NLP-Platform/MyWorkspace/target/com.prescienced.uima-0.1.0-jar-with-dependencies.jar com.prescienced.uima.Test && cd');
-	echo "<div id='div4'><p id='p2'>Process complete</p><br>";
+	echo "<div id='div4'><br><br><a id='sub' href=\"javascript:history.go(-1)\">GO BACK</a><br>";
+	echo "<p id='p2'>Process complete</p><br>";
 	echo "<form id='form1' method='POST' action='../php/new_action.php'>";
 	echo "<input id='sub' type='submit' value='click to view results'></form></div>";
 
