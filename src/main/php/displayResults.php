57d84ddcdf190b36d569842bdf8cfacaaff2bb34
sruthi2498 <vsruthi98@gmail.com>
2017-06-09 09:34:00 +0530

    Create displayResults.php

--- src/main/php/displayResults.php
+++ src/main/php/displayResults.php
@@ -6,7 +6,7 @@
 		
 		
 		<?php
-		echo "<a id='sub' href=\"javascript:history.go(-1)\">GO BACK</a><br>";
+		echo "<div id='div7'><div id='div9'><a id='sub1' href=\"javascript:history.go(-1)\">GO BACK</a></div><br>";
 
 			/*$filelist="/home/sruthi/dev/UIMA-NLP-Platform/MyWorkspace/data/text_files/op/list.txt";
 			$myfile1=fopen($filelist,"r")or die("Unable to open file!");
@@ -25,7 +25,7 @@
 
 			$an=$_POST["Option"];
 			//echo $an;
-			if($an !="DocumentAnnotation"){
+			//if($an !="DocumentAnnotation"){
 		//$an = str_replace(array("\r\n", "\r", "\n"), "<br />", $an);
 		   //echo $an."<br>";
 		    $fileop="/home/sruthi/dev/UIMA-NLP-Platform/MyWorkspace/data/text_files/op/".$an.".csv";
@@ -33,29 +33,32 @@
 			$myfile2=fopen($fileop,"r")or die("Unable to open file!");
 			//fwrite($myfile, $text);
 			//echo "    values"; 
-			echo "<br>";
-			echo "<b>From                   To                  ".$an."<br></b>";
+			echo "<br><div id='div8'><table id='table'>";
+			echo "<tr><th id='th1'>From</th><th id='th1'>To</th><th id='th2'>".$an."</th></tr>";
 			while(!feof($myfile2)) {
 				//echo "    ";
 			 // echo fgets($myfile2) . "<br>";
 				//print_r(fgetcsv($myfile2));
-
+				echo "<tr>";
 				$arr=fgetcsv($myfile2,1024);
-				print $arr[0]."                   ";
-				print $arr[1]."                   ";
-				print $arr[2];
-				echo "<br>";
+				if(count($arr)==3){
+					if($arr[0])echo "<td id='td1'>".$arr[0]."</td>";
+					if($arr[1])echo "<td id='td2'>".$arr[1]."</td>";
+					if($arr[2])echo "<td id='td3'>".$arr[2]."</td></tr>";
+				}
+
 			}
 
 			fclose($myfile2);
 
 		//echo "ok";
-		}
+		//}
 		?>	
 
-
-	
-	
+</table>
+</div>
+<br><br><br>
+</div>	
 </form>
 
 
